ae10aecb1c2d741305c5322d39adce2a077e7823
Amal Prakash <amalkj20000@gmail.com>
2024-12-02 07:41:19 +0530

    some more errors resolved

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,11 +1,11 @@
 import { AuthProps } from '@/types.d';
 import { ProfileTabs } from './ProfileTabs'
 
-const ProfileComplete = ({authToken, setAuthToken, handleLogout}: AuthProps) => {
+const ProfileComplete = (props: AuthProps) => {
     return (
         <div className="flex items-center justify-center h-[calc(100vh-100px)]">
             
-        <ProfileTabs authToken={authToken} handleLogout={handleLogout} setAuthToken={setAuthToken} />
+        <ProfileTabs {...props}/>
 
         </div>
     )
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,12 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
+import { BinaryLike, createHash } from 'crypto';
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+
+}
+
+export function generateHash(inputString: BinaryLike) {
+    return createHash('sha256').update(inputString).digest('hex');
 }
